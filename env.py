c3b95a423ae5e9bbc415439462b96ab6bcab9533
Josh Gendein <joshua@gendein.com>
2020-06-19 12:29:34 -0700

    added random placement of walls, agents, and flag

--- env.py
+++ env.py
@@ -9,27 +9,32 @@ from tf_agents.trajectories import time_step as ts
 tf.compat.v1.enable_v2_behavior()
 
 class CTFEnv(py_environment.PyEnvironment):
-    def __init__(self, grid_size=16, initial_agent_pos=(0, 0), screen_size=512, wall_pos=(5, 5)):
+    def __init__(self, grid_size=16 , screen_size=512, num_walls=4):
         self.grid_size = grid_size
         self.placement_grid = np.zeros((self.grid_size, self.grid_size), dtype=np.uint8)
-        self.initial_agent_pos = initial_agent_pos
+
+        self.num_walls = num_walls
+        self.agent_pos = self.get_agent_pos()
         self.flag_pos = self.get_flag_pos()
-        self.wall_pos = wall_pos
+        # wall_pos is a list because of multiple walls
+        self.wall_pos = self.get_wall_pos()
 
         self._action_spec = array_spec.BoundedArraySpec(
             shape=(), dtype=np.int32, minimum=0, maximum=7, name='action')
 
-        self._observation_spec = array_spec.BoundedArraySpec(
-            shape=(4,), dtype=np.int32, minimum=[0,0,0,0],maximum=[grid_size-1,grid_size-1,grid_size-1,grid_size-1], name='observation')
+        # total params:
+        # x,y agent = 2 +
+        # x,y flag = 2 +
+        # x,y of each wall = 2 * num_walls
+        total_params = 4 + (self.num_walls * 2)
         
-        self._state=[
-            initial_agent_pos[0],
-            initial_agent_pos[1],
-            self.flag_pos[0],
-            self.flag_pos[1],
-            wall_pos[0],
-            wall_pos[1]
-        ] #represent the (row, col, frow, fcol) of the player and the finish
+        self._observation_spec = array_spec.BoundedArraySpec(
+            shape=(total_params,), dtype=np.int32, 
+            minimum= np.zeros((total_params,), dtype=np.int32),
+            maximum= np.full((total_params,), self.grid_size-1, dtype=np.int32), 
+            name='observation')
+
+        self._state= self.get_state()
         
         self._episode_ended = False
 
@@ -45,12 +50,13 @@ class CTFEnv(py_environment.PyEnvironment):
     
     def _reset(self):
         self.step_count = 0
+        self.agent_pos = self.get_agent_pos()
         self.flag_pos = self.get_flag_pos()
+        self.wall_pos = self.get_wall_pos()
         self.reset_grid()
-        self._state=[self.initial_agent_pos[0],self.initial_agent_pos[1],
-                        self.flag_pos[0],self.flag_pos[1]] 
+        self._state= self.get_state()
         self._episode_ended = False
-        return ts.restart(np.array(self._state, dtype=np.int32))
+        return ts.restart(self._state)
 
     def _step(self, action):
         self.step_count += 1
@@ -67,13 +73,15 @@ class CTFEnv(py_environment.PyEnvironment):
                 reward = 100
             else:
                 reward = 0
-            return ts.termination(np.array(self._state, dtype=np.int32), reward)
+            return ts.termination(self._state, reward)
         else:
             return ts.transition(
-                np.array(self._state, dtype=np.int32), reward=0, discount=0.9)
+                self._state, reward=0, discount=0.9)
     
     def move(self, action):
+        # Get the current position of the agent.
         row, col = self._state[0],self._state[1]
+        # Set the position in the grid to 0 because we are about to move away from it.
         self.placement_grid[row, col] = 0
         if action == 0: #down
             if row - 1 >= 0 and self.placement_grid[row-1][col] != 3:
@@ -110,32 +118,13 @@ class CTFEnv(py_environment.PyEnvironment):
         return row==frow and col==fcol
     
     def reset_grid(self):
-        self.placement_grid[self._state[0]][self._state[1]] = 0
-        self.placement_grid[self._state[2]][self._state[3]] = 0
-        self.placement_grid[0][5] = 3
-        self.placement_grid[1][5] = 3
-        self.placement_grid[2][5] = 3
-        self.placement_grid[3][5] = 3
-        self.placement_grid[4][5] = 3
-        self.placement_grid[5][5] = 3
-       
-        '''
-        self.placement_grid[3][9] = 3
-        self.placement_grid[4][9] = 3
-        self.placement_grid[5][9] = 3
-        self.placement_grid[6][9] = 3
-        self.placement_grid[7][9] = 3
-        self.placement_grid[8][9] = 3
-        self.placement_grid[9][9] = 3
-        self.placement_grid[10][9] = 3
-        self.placement_grid[11][9] = 3
-        self.placement_grid[12][9] = 3
-        self.placement_grid[13][9] = 3
-        self.placement_grid[14][9] = 3
-        self.placement_grid[15][9] = 3
-        '''
-        self.placement_grid[self.initial_agent_pos[0]][self.initial_agent_pos[1]] = 1
+        for i in range(0, len(self._state), 2):
+            self.placement_grid[self._state[i]][self._state[i+1]] = 0
+
+        self.placement_grid[self.agent_pos[0]][self.agent_pos[1]] = 1
         self.placement_grid[self.flag_pos[0]][self.flag_pos[1]] = 2
+        for i in range(0, len(self.wall_pos), 2):
+            self.placement_grid[self.wall_pos[i]][self.wall_pos[i+1]] = 3
     
     def render(self, mode='rgb_array'):
         frame = np.full((self.screen_size, self.screen_size, 3), 255, dtype=np.uint8)
@@ -158,6 +147,34 @@ class CTFEnv(py_environment.PyEnvironment):
             for j in range(l, r):
                 frame[i, j] = color
     
+    def get_state(self):
+        state = [
+            self.agent_pos[0],
+            self.agent_pos[1],
+            self.flag_pos[0],
+            self.flag_pos[1],
+        ]
+        state.extend(self.wall_pos)
+
+        return np.array(state, dtype=np.int32)
+
+    def get_agent_pos(self):
+        return (np.random.randint(self.grid_size), np.random.randint(self.grid_size))
+        # return (0, 0)
+
     def get_flag_pos(self):
-        # return (np.random.randint(self.grid_size), self.grid_size - 1)
-        return (7, 15)
+        return (np.random.randint(self.grid_size), np.random.randint(self.grid_size))
+        # return (15, 15)
+
+    def get_wall_pos(self):
+        wall_pos = []
+        for _ in range(self.num_walls):
+            found = False
+            while not found:
+                x = np.random.randint(self.grid_size)
+                y = np.random.randint(self.grid_size)
+                if x != self.agent_pos[0] and y != self.agent_pos[1] and x != self.flag_pos[0] and y != self.flag_pos[1]:
+                    wall_pos.append(np.random.randint(self.grid_size))
+                    wall_pos.append(np.random.randint(self.grid_size))
+                    found = True
+        return wall_pos
