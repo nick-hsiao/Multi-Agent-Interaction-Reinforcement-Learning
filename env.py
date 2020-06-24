6003bc23e7a05789eecb336c62a713e412a24efd
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2020-06-24 12:30:16 -0700

    Update env.py

--- env.py
+++ env.py
@@ -202,8 +202,4 @@ class CTFEnv(py_environment.PyEnvironment):
                     wall_pos.append(x)
                     wall_pos.append(y)
                     found = True
-<<<<<<< HEAD
         return wall_pos
-=======
-        return wall_pos
->>>>>>> c89e0a841711596e02170e6a148939361bd16c1c
