74ca7dd4c4fba48cd7d608f9ba51dba9c042a84f
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2020-07-15 12:13:10 -0700

    Added support for taking user input

--- game.py
+++ game.py
@@ -2,21 +2,31 @@ import sys
 import os
 import tkinter as tk
 from PIL import Image, ImageTk
-
+import subprocess
 
 #User interface of application, written by Aliaksandr Nenartovich
 def play():
-	os.system("python3 train.py")
+	subprocess.call(["python3", "train.py", size_entered.get(), walls_entered.get(), agents_entered.get()])
 
 def make_video():
-	os.system("python3 evaluate.py")
+	subprocess.call(["python3", "evaluate.py", size_entered.get(), walls_entered.get(), agents_entered.get()])
 
 root = tk.Tk()
-root.geometry("700x550")
+root.geometry("850x610")
 root.title("Multi-Agent Interaction")
 
 def quit():
 	root.destroy()
+
+grid_label = tk.Label(root, text = "Grid size:")
+grid = tk.StringVar()
+size_entered = tk.Entry(root, width=4, textvariable = grid)
+wall_label = tk.Label(root, text = "Number of walls:")
+walls = tk.StringVar()
+walls_entered = tk.Entry(root, width = 4, textvariable = walls)
+agent_label = tk.Label(root, text = "Number of Agents on Each Team:")
+agents = tk.StringVar()
+agents_entered = tk.Entry(root, width=4, textvariable = agents)
 
 btn1 = tk.Button(root, text="Run Program", command=play)
 video = tk.Button(root, text="Create Video", command=make_video)
@@ -27,14 +37,25 @@ background_label = tk.Label(root, image=background_image)
 
 #Add text
 text = tk.Label(root, text="Welcome to Multi-Agent Interaction!")
+enter = tk.Label(root, text="Please Fill in All Fields Below:")
 
 #Change text size, from https://stackoverflow.com/questions/30685308/how-do-i-change-the-text-size-in-a-label-widget-python-tkinter
 text.config(font=("Arial", 25))
+enter.config(font=("Arial", 20))
 
 text.pack(pady = (0,0))
 background_label.pack()
+
+enter.pack(pady=(50,0))
+grid_label.pack(padx = (50,0), side = tk.LEFT)
+size_entered.pack(padx = (5,40), side = tk.LEFT)
+wall_label.pack(padx = 0, side = tk.LEFT)
+walls_entered.pack(padx = (5,40), side = tk.LEFT)
+agent_label.pack(padx = 0, side = tk.LEFT)
+agents_entered.pack(padx = 0, side = tk.LEFT)
+
 btn1.pack(pady=(0,10))
 video.pack(pady=(0,10))
-quit.pack(pady=(0,20))
+quit.pack(pady=(0,10))
 
 root.mainloop()
