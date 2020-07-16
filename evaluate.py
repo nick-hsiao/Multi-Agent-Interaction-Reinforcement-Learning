b6b88d30a08c090049ce379f91e92f36c80bfe94
richardphamsjsu2016 <42747446+richardphamsjsu2016@users.noreply.github.com>
2020-07-15 21:03:25 -0700

    GUI Update

--- evaluate.py
+++ evaluate.py
@@ -4,16 +4,35 @@ from env import CTFEnv
 import imageio
 import sys
 
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
 #Code from the following links were used: https://www.tensorflow.org/agents/tutorials/1_dqn_tutorial
 
 model_path = "policies/policy_static_goal_dynamic_reward"
 filename = "videos/example_training"
 filename = filename + ".mp4"
 
-grid_size = int(sys.argv[1])
-num_walls = int(sys.argv[2])
-num_agents = int(sys.argv[3])
-def_agents = int(sys.argv[4])
+if(str.isdigit(sys.argv[1])):
+    grid_size = int(sys.argv[1])
+else:
+    grid_size = 0
+
+if(str.isdigit(sys.argv[2])):
+    num_walls = int(sys.argv[2])
+else:
+    num_walls = 0
+
+if(str.isdigit(sys.argv[3])):
+    num_agents = int(sys.argv[3])
+else:
+    num_agents = 0
+
+if(str.isdigit(sys.argv[4])):
+    def_agents = int(sys.argv[4])
+else:
+    def_agents = 0
 
 py_env = wrappers.TimeLimit(CTFEnv(grid_size, 512, num_walls, num_agents, def_agents), duration=100)
 
@@ -30,3 +49,10 @@ with imageio.get_writer(filename, fps=15) as video:
             action_step = policy.action(time_step)
             time_step = env.step(action_step.action)
             video.append_data(py_env.render())
+
+root = tk.Tk()
+root.geometry("300x100")
+root.title("Complete")
+text = tk.Label(root, text="Video has been generated! \n Please check the videos folder!")
+text.pack(pady = (0,0))
+root.mainloop()
--- train.py
+++ train.py
@@ -47,10 +47,26 @@ eval_interval = 1000  # @param {type:"integer"}
 #Training environment, written by Josh Gendein, borrowed and modified from the tutorial in https://www.tensorflow.org/agents/tutorials/1_dqn_tutorial
 #Simulation will last 200 steps
 
-grid_size = int(sys.argv[1])
-num_walls = int(sys.argv[2])
-num_agents = int(sys.argv[3])
-def_agents = int(sys.argv[4])
+if(str.isdigit(sys.argv[1])):
+    grid_size = int(sys.argv[1])
+else:
+    grid_size = 0
+
+if(str.isdigit(sys.argv[2])):
+    num_walls = int(sys.argv[2])
+else:
+    num_walls = 0
+
+if(str.isdigit(sys.argv[3])):
+    num_agents = int(sys.argv[3])
+else:
+    num_agents = 0
+
+if(str.isdigit(sys.argv[4])):
+    def_agents = int(sys.argv[4])
+else:
+    def_agents = 0
+
 c = CTFEnv(grid_size, 512, num_walls, num_agents, def_agents)
 
 train_py_env = wrappers.TimeLimit(c, duration=200)
